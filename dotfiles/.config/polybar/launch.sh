ec7e54a8d41300d7c5a8e190615fff9aefe91038
joao paulo <jpaulo@rrnwsri61.tre-rn.jus.br>
2020-10-19 18:33:35 -0300

    Adiciona suporte a 2 monitores hdmi

--- dotfiles/.config/polybar/launch.sh
+++ dotfiles/.config/polybar/launch.sh
@@ -6,5 +6,10 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch polybar
-polybar example -c $HOME/.config/polybar/config &
+# Launch principal polybar
+polybar right -c $HOME/.config/polybar/config &
+
+# Launch second polybar
+if [[ $(xrandr -q | grep -w connected | wc -l) -eq 2 ]]; then
+    polybar left -c $HOME/.config/polybar/config &
+fi
