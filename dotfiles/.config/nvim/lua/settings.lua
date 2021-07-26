d73917102c90aadd8ed367f5bcbbf9d6ca37618d
Joao @home <16942018+nnorakk@users.noreply.github.com>
2021-07-26 15:50:46 -0300

    settings.lua: refatora vim.o -> vim.opt, adiciona opcao shortmess

--- dotfiles/.config/nvim/lua/settings.lua
+++ dotfiles/.config/nvim/lua/settings.lua
@@ -9,26 +9,28 @@
 -- vim.o.fillchars="vert:│" -- Modifica o caracter separador vertical (U+2503, UTF-8: E2 94 83)
 -- vim.o.backspace = 'indent,eol,start' -- comportamento backspace
 
-vim.o.clipboard = 'unnamedplus' -- forca registros * e + em yank
-vim.o.foldmethod = 'indent' -- Usa folding atraves da identacao
-vim.o.foldlevel = 99  -- Abre sem folding
-vim.o.tabstop = 4 -- expande tabs em espacos
-vim.o.softtabstop = 4 -- expande tabs em espacos
-vim.o.shiftwidth = 4 -- expande tabs em espacos
-vim.o.expandtab = true -- expande tabs em espacos
-vim.o.number = true -- Seta line
-vim.o.relativenumber = true -- Seta relativenumber
-vim.o.cursorline = true -- Habilita highlight na linha atual
-vim.o.splitbelow = true -- " Preferencias ao splitar janelas
-vim.o.splitright = true -- " Preferencias ao splitar janelas
-vim.o.hidden = true -- Permite saltar para buffers nao salvos
-vim.o.showcmd = false -- Nao imprime comando na ultima linha
-vim.o.termguicolors = true -- 
-vim.o.mouse = 'nv' -- habilita mouse modos normal e visual
-vim.o.completeopt = "menuone,noselect" --https://github.com/hrsh7th/nvim-compe
+vim.opt.clipboard = 'unnamedplus' -- forca registros * e + em yank
+vim.opt.foldmethod = 'indent' -- Usa folding atraves da identacao
+vim.opt.foldlevel = 99  -- Abre sem folding
+vim.opt.tabstop = 4 -- expande tabs em espacos
+vim.opt.softtabstop = 4 -- expande tabs em espacos
+vim.opt.shiftwidth = 4 -- expande tabs em espacos
+vim.opt.expandtab = true -- expande tabs em espacos
+vim.opt.number = true -- Seta line
+vim.opt.relativenumber = true -- Seta relativenumber
+vim.opt.cursorline = true -- Habilita highlight na linha atual
+vim.opt.splitbelow = true -- " Preferencias ao splitar janelas
+vim.opt.splitright = true -- " Preferencias ao splitar janelas
+vim.opt.hidden = true -- Permite saltar para buffers nao salvos
+vim.opt.showcmd = false -- Nao imprime comando na ultima linha
+vim.opt.showmode = false -- Nao indica "INSERT","REPLACE","VISUAL" mode
+vim.opt.termguicolors = true --
+vim.opt.mouse = 'nv' -- habilita mouse modos normal e visual
+vim.opt.completeopt = "menuone,noselect" --https://github.com/hrsh7th/nvim-compe
+vim.opt.shortmess = vim.opt.shortmess + 'I' -- nao aparece tela inicial
 
 vim.g.gruvbox_italic = 1 -- verificar pq aqui antes de tudo
 vim.g.colors_name = 'gruvbox'
 
-vim.cmd('highlight link CompeDocumentation NormalFloat')
-vim.cmd('highlight Comment gui=italic')
+-- vim.cmd('highlight link CompeDocumentation NormalFloat')
+-- vim.cmd('highlight Comment gui=italic')
