43a13d801951e1d69b83d0809e38ed118f9672ed
Joao @home <16942018+nnorakk@users.noreply.github.com>
2021-08-02 10:14:53 -0300

    alias.sh: Ajusta criacao de alias que nao sao validos em bash

--- dotfiles/.config/shell.d/alias.sh
+++ dotfiles/.config/shell.d/alias.sh
@@ -43,11 +43,13 @@ alias l.='ls -d .* --color=auto'
 
 # aliases uteis para saltar entre locais ja visitados
 alias dirs='dirs -v'
-alias -1='cd -1'
-alias -2='cd -2'
-alias -3='cd -3'
-alias -4='cd -4'
-alias -5='cd -5'
+if [ -z $BASH ]; then
+    alias -1='cd -1'
+    alias -2='cd -2'
+    alias -3='cd -3'
+    alias -4='cd -4'
+    alias -5='cd -5'
+fi
 
 # Aliases melhorar minha vida com um teclado ruim
 alias suudo='sudo'
