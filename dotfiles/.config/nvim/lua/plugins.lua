9fc6474f5fe8752ebb31af8df4c23291d7a357a9
Joao @home <16942018+nnorakk@users.noreply.github.com>
2021-08-02 12:43:02 -0300

    plugins.lua: refatora

--- dotfiles/.config/nvim/lua/plugins.lua
+++ dotfiles/.config/nvim/lua/plugins.lua
@@ -5,7 +5,7 @@
 
 return require('packer').startup(function()
 -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+  use {'wbthomason/packer.nvim'}
   use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
   use { 'nvim-telescope/telescope.nvim', requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}} }
   use {'neovim/nvim-lspconfig'}
