3895d6595d00dfb142d453524e7b50ae454db82f
Joao @home <16942018+nnorakk@users.noreply.github.com>
2021-10-24 21:08:03 -0300

    Adiciona atalhos de teclado ao usar vim-snip

--- dotfiles/.config/nvim/plugin/plugins_config/nvim-cmp.lua
+++ dotfiles/.config/nvim/plugin/plugins_config/nvim-cmp.lua
@@ -41,3 +41,18 @@ cmp.setup {
       })
   }
 }
+
+-- from: https://github.com/bluz71/dotfiles/blob/master/vim/lua/vsnip-config.lua
+-- local g = vim.g
+-- local fn = vim.fn
+local map = vim.api.nvim_set_keymap
+
+-- g.vsnip_snippet_dir = fn.expand('~/dotfiles/vim/vsnip')
+
+local opts = {expr = true, silent = true}
+map('i', '<C-j>', 'vsnip#available(1) ? "<Plug>(vsnip-expand-or-jump)" : "<C-j>"', opts)
+map('i', '<C-k>', 'vsnip#jumpable(-1) ? "<Plug>(vsnip-jump-prev)" : "<C-k>"', opts)
+
+local opts = {noremap = true, silent = true}
+-- Insert mode snippet completion mapping - '<Control-s>'
+map('i', '<C-s>', '<C-r>=snippets#Complete()<CR>', opts)
