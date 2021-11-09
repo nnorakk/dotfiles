d53b372d68f1b9e756c7097f3e7fa324683c2474
Joao @home <16942018+nnorakk@users.noreply.github.com>
2021-11-09 07:16:57 -0300

    Refatora e adiciona habilita/desabilita indentline ao ganhar/perder foco

--- dotfiles/.config/nvim/plugin/lua/highlightActiveInactiveWindows.lua
+++ dotfiles/.config/nvim/plugin/lua/highlightActiveInactiveWindows.lua
@@ -2,22 +2,24 @@
 vim.cmd('hi InactiveWindow guibg=#333333')
 vim.cmd('hi link ActiveWindow Normal')
 
-_G._autocommands = {} -- or wherever you prefer
+autocommands = {} -- or wherever you prefer
 
-_G._autocommands.win_enter = function()
+autocommands.win_enter = function()
     vim.cmd('set winhighlight=Normal:ActiveWindow,NormalNC:InactiveWindow')
+    vim.cmd [[IndentBlanklineEnable]]
     vim.opt.relativenumber = true
 end
 
-_G._autocommands.win_leave = function()
+autocommands.win_leave = function()
     vim.cmd('set winhighlight=Normal:InactiveWindow,NormalNC:InactiveWindow')
+    vim.cmd [[IndentBlanklineDisable]]
     vim.opt.relativenumber = false
 end
 
 vim.api.nvim_exec([[
 augroup line_return
     au!
-    au WinEnter,FocusGained * lua _autocommands.win_enter()
-    au WinLeave,FocusLost,BufLeave * lua _autocommands.win_leave()
+    au WinEnter,FocusGained * lua autocommands.win_enter()
+    au WinLeave,FocusLost,BufLeave * lua autocommands.win_leave()
 augroup END
 ]], false)
