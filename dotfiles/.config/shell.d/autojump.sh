abdcbfe2e95741a89a8232ff9597847692a4ed96
Joao <jpaulo@siverstar.home.local>
2021-11-18 07:26:13 -0300

    Adiciona pacote autojump no Mac

--- dotfiles/.config/shell.d/autojump.sh
+++ dotfiles/.config/shell.d/autojump.sh
@@ -1,1 +1,5 @@
 [[ -s /etc/profile.d/autojump.sh ]] && source /etc/profile.d/autojump.sh
+
+if [[ $(uname) == 'Darwin' ]]; then
+    [ -f $(brew --prefix)/etc/profile.d/autojump.sh ] && . $(brew --prefix)/etc/profile.d/autojump.sh
+fi
