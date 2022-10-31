f7ebad4b525bfc04ff0b59396e8d334b487c5a9e
Joao @home <16942018+nnorakk@users.noreply.github.com>
2022-10-31 15:39:08 -0300

    adiciona plugin nvim mini.lua

--- dotfiles/.config/nvim/lua/plugins.lua
+++ dotfiles/.config/nvim/lua/plugins.lua
@@ -1,9 +1,13 @@
 return require('packer').startup({function()
 -- Packer can manage itself
   use {'wbthomason/packer.nvim'}
+
   use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+
   use { 'nvim-telescope/telescope.nvim', requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}} }
+
   use {'neovim/nvim-lspconfig'}
+
   use {'hrsh7th/nvim-cmp', requires = {
       {'hrsh7th/cmp-nvim-lsp'}, -- funciona aqui comentario?
       {'hrsh7th/cmp-buffer'},
@@ -13,12 +17,20 @@ return require('packer').startup({function()
       {'hrsh7th/vim-vsnip-integ'},
       {'onsails/lspkind-nvim'}}
   }
+
   use {'windwp/nvim-autopairs'}
+
   use {'lewis6991/gitsigns.nvim', requires = {'nvim-lua/plenary.nvim'},}
+
   use {'lukas-reineke/indent-blankline.nvim'}
+
   use {'rafamadriz/friendly-snippets'}
+
   use {'mrjones2014/smart-splits.nvim'}
+
   use {'voldikss/vim-floaterm'}
+
+  use {'echasnovski/mini.nvim'}
   -- -- Ja utilizados historicamente com o vim
   use {'morhetz/gruvbox'} -- tema
   use {'itchyny/lightline.vim', requires = {'itchyny/vim-gitbranch'}} -- statusline
