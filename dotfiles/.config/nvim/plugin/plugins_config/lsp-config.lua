869000175f900d724337b6a687aebc5e9ffb1e9c
Joao @home <16942018+nnorakk@users.noreply.github.com>
2023-01-27 21:05:08 -0300

    nvim: refatora

--- dotfiles/.config/nvim/plugin/plugins_config/lsp-config.lua
+++ dotfiles/.config/nvim/plugin/plugins_config/lsp-config.lua
@@ -1,38 +1,39 @@
 -- local nvim_lsp = require('lspconfig')
-local opts = { noremap=true, silent=true }
+local opts = {noremap = true, silent = true}
 
+vim.g.diagnostics_visible = true
+
+function _G.toggle_diagnostics()
+    if vim.g.diagnostics_visible then
+        vim.g.diagnostics_visible = false
+        vim.diagnostic.disable()
+    else
+        vim.g.diagnostics_visible = true
+        vim.diagnostic.enable()
+    end
+end
+
+vim.keymap.set('n', '<Leader>dh', ':call v:lua.toggle_diagnostics()<CR>', opts)
 vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
 vim.keymap.set('n', '<space>dq', vim.diagnostic.setloclist, opts)
-vim.g.diagnostics_visible = true
-function _G.toggle_diagnostics()
-  if vim.g.diagnostics_visible then
-    vim.g.diagnostics_visible = false
-    vim.diagnostic.disable()
-  else
-    vim.g.diagnostics_visible = true
-    vim.diagnostic.enable()
-  end
-end
-vim.api.nvim_buf_set_keymap(0, 'n', '<Leader>dh', ':call v:lua.toggle_diagnostics()<CR>', {silent=true, noremap=true})
 
--- Use an on_attach function to only map the following keys
--- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
     -- Enable completion triggered by <c-x><c-o>
     vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
     -- Mappings.
     -- See `:help vim.lsp.*` for documentation on any of the below functions
-    local bufopts = { noremap=true, silent=true, buffer=bufnr }
+    local bufopts = {noremap = true, silent = true, buffer = bufnr}
     vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
     vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
     vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+    vim.keymap.set('n', 'gs', vim.lsp.buf.signature_help, bufopts)
     vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder,
+                   bufopts)
     vim.keymap.set('n', '<space>wl', function()
         print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
     end, bufopts)
@@ -40,22 +41,29 @@ local on_attach = function(client, bufnr)
     vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
     vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
     vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-    vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
-
+    vim.keymap.set('n', '<space>f',
+                   function() vim.lsp.buf.format {async = true} end, bufopts)
 
 end
 
 local lsp_flags = {
     -- This is the default in Nvim 0.7+
-    debounce_text_changes = 150,
+    debounce_text_changes = 150
 }
 
-require('lspconfig')['pylsp'].setup{
-    on_attach = on_attach,
-    flags = lsp_flags,
-}
+-- python lsp
+require('lspconfig')['pylsp'].setup {on_attach = on_attach, flags = lsp_flags}
 
-require('lspconfig')['sumneko_lua'].setup{
+-- lua lsp
+require('lspconfig')['sumneko_lua'].setup {
+    settings = {
+        Lua = {
+            diagnostics = {
+                -- Get the language server to recognize the `vim` global
+                globals = {'vim'}
+            }
+        }
+    },
     on_attach = on_attach,
-    flags = lsp_flags,
+    flags = lsp_flags
 }
