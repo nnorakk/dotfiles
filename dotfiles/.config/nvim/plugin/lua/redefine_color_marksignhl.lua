68e14c1b41756f5e39fb492d9ebc3666fbd5a3ca
Joao @home <16942018+nnorakk@users.noreply.github.com>
2023-04-18 08:43:40 -0300

    nvim: corrige augroup que redefina highlight-group MarkSignHL

--- dotfiles/.config/nvim/plugin/lua/redefine_color_marksignhl.lua
+++ dotfiles/.config/nvim/plugin/lua/redefine_color_marksignhl.lua
@@ -1,6 +1,6 @@
 vim.api.nvim_exec([[
 augroup redefine_group_marksignhl
     au!
-    au ColorScheme * silent! luavim.api.nvim_set_hl(0, "MarkSignHL", {link = "GruvboxBlueSign"})
+    au ColorScheme * silent! lua vim.api.nvim_set_hl(0, "MarkSignHL", {link = "GruvboxBlueSign"})
 augroup END
 ]], false)
