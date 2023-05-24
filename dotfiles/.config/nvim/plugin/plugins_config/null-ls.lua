ff0154c4d54236bd47d2d3631b1f5558b6402896
Joao @home <16942018+nnorakk@users.noreply.github.com>
2023-05-24 09:57:04 -0300

    nvim: Configura shfmt como formatter de arquivos sh

--- dotfiles/.config/nvim/plugin/plugins_config/null-ls.lua
+++ dotfiles/.config/nvim/plugin/plugins_config/null-ls.lua
@@ -5,6 +5,7 @@ null_ls.setup({
         null_ls.builtins.formatting.isort,
         null_ls.builtins.formatting.black,
         null_ls.builtins.formatting.lua_format,
+        null_ls.builtins.formatting.shfmt.with({ extra_args = { "-i", "2", "-ci" }, }),
         null_ls.builtins.diagnostics.flake8,
     },
 })
