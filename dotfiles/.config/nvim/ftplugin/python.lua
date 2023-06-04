d8ef4d3afc42a168ef4df579c20e348983b31dfc
Joao @home <16942018+nnorakk@users.noreply.github.com>
2023-06-04 15:44:07 -0300

    nvim: Modifica arquivo para usar completamente lua

--- dotfiles/.config/nvim/ftplugin/python.lua
+++ dotfiles/.config/nvim/ftplugin/python.lua
@@ -3,6 +3,9 @@ vim.wo.foldmethod='indent'
 vim.wo.foldlevel=99
 
 vim.opt.listchars      = {trail='•',eol='↲', tab='▸\''}
-vim.cmd [[normal zM]]
-vim.cmd [[let g:python3_host_prog = '$VIRTUAL_ENV/bin/python3']]
-vim.cmd [[autocmd BufWritePre *.py lua vim.lsp.buf.format()]]
+vim.api.nvim_command('autocmd BufEnter *.py normal! zM')
+vim.g.python3_host_prog = '$VIRTUAL_ENV/bin/python3'
+vim.api.nvim_command('autocmd BufWritePre *.py lua vim.lsp.buf.format()')
+-- vim.cmd [[autocmd BufEnter *.py normal zM]]
+-- vim.cmd [[let g:python3_host_prog = '$VIRTUAL_ENV/bin/python3']]
+-- vim.cmd [[autocmd BufWritePre *.py lua vim.lsp.buf.format()]]
