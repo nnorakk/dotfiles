3df15a19cdffd480f05423bb9d3617e6bb8dcd7f
Joao @home <16942018+nnorakk@users.noreply.github.com>
2023-06-13 16:46:43 -0300

    nvim: Modifica o evento para BufWinEnter para aplicar zM

--- dotfiles/.config/nvim/ftplugin/python.lua
+++ dotfiles/.config/nvim/ftplugin/python.lua
@@ -3,7 +3,7 @@ vim.wo.foldmethod='indent'
 vim.wo.foldlevel=99
 
 vim.opt.listchars      = {trail='•',eol='↲', tab='▸\''}
-vim.api.nvim_command('autocmd BufEnter *.py normal! zM')
+vim.api.nvim_command('autocmd BufWinEnter *.py normal! zM')
 vim.g.python3_host_prog = '$VIRTUAL_ENV/bin/python3'
 vim.api.nvim_command('autocmd BufWritePre *.py lua vim.lsp.buf.format()')
 -- vim.cmd [[autocmd BufEnter *.py normal zM]]
