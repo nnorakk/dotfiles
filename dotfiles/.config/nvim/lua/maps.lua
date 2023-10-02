d19b00acd2d1300bf65f61ecb19430e93963a7e9
Joao @home <16942018+nnorakk@users.noreply.github.com>
2023-10-01 22:36:15 -0300

    Refatora arquivos de configuracao tornando a configuracao mais lua que vimscript

--- dotfiles/.config/nvim/lua/maps.lua
+++ dotfiles/.config/nvim/lua/maps.lua
@@ -3,6 +3,8 @@
 local map = vim.api.nvim_set_keymap
 local options = { noremap = true, silent = true }
 
+-- normal mappings
+--
 -- map the leader key
 map('n', '<Space>', '', {})
 
@@ -25,21 +27,75 @@ map('n', '<Leader>cd', ':cd %:p:h<CR>', options)
 -- https://github.com/omerxx/vim-notebook
 map('n', '<Leader>yyb', 'V}y}p<CR>', options)
 
--- normal mapping
+-- navegacao entre as janelas
 map('n', '<c-h>', '<c-w>h', options)
 map('n', '<c-j>', '<c-w>j', options)
 map('n', '<c-k>', '<c-w>k', options)
 map('n', '<c-l>', '<c-w>l', options)
 
--- insert mapping
+-- cria linhas abaixo e acima voltando para o normal mode
+map('n', '<Leader>o', 'm`o<Esc>k``', options)
+map('n', '<Leader>O', 'm`O<Esc>j``', options)
+
+-- navega entre linhas virtuais e reais naturalmente
+map('n', 'j', 'gj', options)
+map('n', 'k', 'gk', options)
+
+-- from https://github.com/ThePrimeagen/.dotfiles
+-- Centraliza cursor na busca no centro da tela
+map('n', 'n',  'nzzzv', options)
+map('n', 'N',  'Nzzzv', options)
+map('n', 'J', 'mzJ`z', options)
+
+-- abre ou fecha fold
+map('n', '<Leader><Leader>', 'za', options)
+
+-- equaliza tamanho das janelas
+map('n', '<Leader>=', '<C-W>=', options)
+-- converte de vertical para horizontal as janelas
+map('n', '<Leader>-', '<c-w>t<c-w>K', options)
+-- converte de horizontal para vertical as janelas
+map('n', '<Leader>\\', '<c-w>t<c-w>H', options)
+map('n', '<Leader>|', '<c-w>t<c-w>H', options)
+
+-- inverte janelas tenta emular o atalho do tmux
+map('n', '<Leader>{', '<c-w>r', options)
+map('n', '<Leader>}', '<c-w>r', options)
+
+-- atalhos para navegacao entre buffers
+map('n', '<leader>n', ':bnext<CR>', options)
+map('n', '<leader>p', ':bprevious<CR>', options)
+map('n', '<leader>ls', ':ls<CR>', options)
+
+-- insert mappings
+--
 map('i', 'jk', '<Esc>', {})
 map('i', '<C-u>', '<Esc>ui', {})
 map('i', '<C-r>', '<Esc><C-r>i', {})
+map('i', '<C-a>', '<C-O>0', {})
+map('i', '<C-e>', '<C-O>$', {})
 
--- command mapping
+-- Command Mappings
+--
 map('c', '<c-e>', '<End>', {})
 map('c', '<c-a>', '<Home>', {})
 
+
+-- Visual Mappings
+--
+-- Escape to normal mode
+map('v', 'jk', '<Esc>', {})
+
+-- permite usar shift varias vezes sem perder a selecao
+map('v', '<', '<gv', {})
+map('v', '>', '>gv', {})
+
+-- copia bloco permanecendo no final do bloco
+-- map('v', 'gy', "<ESC>`>magvy'", {})
+-- xnoremap gy <ESC>`>magvy'a
+
+-- Plugins Mappings
+--
 -- telescope plugin
 map('n', '<Leader>ff', ':lua require("telescope.builtin").find_files({follow=true})<cr>', options)
 map('n', '<Leader>fg', ':lua require("telescope.builtin").live_grep()<cr>', options)
--- dotfiles/.config/nvim/plugin/mappings/insertmap.vim
+++
@@ -1,5 +0,0 @@
-inoremap jk <Esc>
-inoremap kj <Esc>
-
-inoremap <C-A> <C-O>0
-inoremap <C-E> <C-O>$
