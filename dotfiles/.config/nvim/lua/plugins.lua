db3863a9d6cbaf494964f6cc3638d77e71bbe665
Joao @home <16942018+nnorakk@users.noreply.github.com>
2023-10-04 10:26:09 -0300

    nvim: adiciona plugin which-key.nvim

--- dotfiles/.config/nvim/lua/plugins.lua
+++ dotfiles/.config/nvim/lua/plugins.lua
@@ -35,6 +35,19 @@ require('lazy').setup({
         },
     },
     {
+        "folke/which-key.nvim", -- gerencia atalhos
+        event = "VeryLazy",
+        init = function()
+            vim.o.timeout = true
+            vim.o.timeoutlen = 300
+        end,
+        opts = {
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
+        }
+    },
+    {
         'glepnir/lspsaga.nvim',
         branch = "main",
         config = function()
