99649e856a53cd5a7e287b641399350c37aaa876
Joao @home <16942018+nnorakk@users.noreply.github.com>
2023-11-20 07:36:21 -0300

    nvim: Substitui o mecanismo de snippet de vim-snip para luasnip

--- dotfiles/.config/nvim/lua/plugins.lua
+++ dotfiles/.config/nvim/lua/plugins.lua
@@ -60,10 +60,15 @@ require('lazy').setup({
             { 'hrsh7th/cmp-nvim-lsp' }, -- funciona aqui comentario?
             { 'hrsh7th/cmp-buffer' },
             { 'hrsh7th/cmp-path' },
-            { 'hrsh7th/cmp-vsnip' },
-            { 'hrsh7th/vim-vsnip' },
-            { 'hrsh7th/vim-vsnip-integ' },
             { 'onsails/lspkind-nvim' } }
+    },
+    {
+        "L3MON4D3/LuaSnip",
+        dependencies = { 'saadparwaiz1/cmp_luasnip' },
+        -- follow latest release.
+        version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+        -- install jsregexp (optional!).
+        build = "make install_jsregexp"
     },
     'windwp/nvim-autopairs',       -- automaticamente [],{},{}
     {
--- dotfiles/.config/nvim/plugin/plugins_config/nvim-cmp.lua
+++ dotfiles/.config/nvim/plugin/plugins_config/nvim-cmp.lua
@@ -1,58 +1,93 @@
-local cmp = require 'cmp'
-
+local cmp = require('cmp')
+local ls = require("luasnip")
 local lspkind = require('lspkind')
 
+-- carrega snippets friendly-snippets
+require('luasnip.loaders.from_vscode').lazy_load()
+
+-- carrega custons snippets desse diretorio '~/.vsnip'
+require("luasnip.loaders.from_lua").load({ paths = "~/.vsnip/" })
+
+-- config luasnip plugin
+ls.config.set_config({ -- Setting LuaSnip config
+    -- Enable autotriggered snippets
+    enable_autosnippets = true,
+})
+
+-- config cmp plugin
 cmp.setup {
-  snippet = {
-      expand = function(args)
-        vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-    end,
-  },
-  mapping = {
-    ['<C-p>'] = cmp.mapping.select_prev_item(),
-    ['<C-n>'] = cmp.mapping.select_next_item(),
-    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.close(),
-    ['<CR>'] = cmp.mapping.confirm {
-      behavior = cmp.ConfirmBehavior.Replace,
-      select = true,
+    snippet = {
+        expand = function(args)
+            require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+            -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+        end,
     },
-    ['<Tab>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 's' }),
-    ["<S-Tab>"] = cmp.mapping.select_prev_item({behavior=cmp.SelectBehavior.Insert}),
-  },
-  sources = {
-    { name = 'nvim_lsp' },
-    { name = 'buffer', keyword_length = 5 }, -- apresenta sugestoes depois de 5 caracteres
-    { name = 'path' },
-    { name = 'vsnip' },
-  },
-  formatting = {
-      format = lspkind.cmp_format({
-          with_text = false,
-          maxwidth = 50,
-          menu = {
-              buffer = "[buf]",
-              nvim_lsp = "[LSP]",
-              path = "[path]",
-              vsnip = "[snip]",
-          }
-      })
-  }
+    window = {
+        -- completion = cmp.config.window.bordered(),
+        -- documentation = cmp.config.window.bordered(),
+    },
+    mapping = {
+        ['<C-p>'] = cmp.mapping.select_prev_item(),
+        ['<C-n>'] = cmp.mapping.select_next_item(),
+        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.close(),
+        ['<CR>'] = cmp.mapping.confirm {
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true,
+        },
+        ['<Tab>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 's' }),
+        ["<S-Tab>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+    },
+    sources = {
+        { name = 'nvim_lsp' },
+        { name = 'buffer',  keyword_length = 5 }, -- apresenta sugestoes depois de 5 caracteres
+        { name = 'luasnip' },
+        { name = 'path' },
+        -- { name = 'vsnip' },
+    },
+    formatting = {
+        format = lspkind.cmp_format({
+            with_text = false,
+            maxwidth = 50,
+            mode = 'symbol',
+            menu = {
+                buffer = "[buf]",
+                nvim_lsp = "[LSP]",
+                luasnip = "[snip]",
+                path = "[path]",
+                -- vsnip = "[snip]",
+            }
+        })
+    }
 }
+
+-- luasnip atalhos
+vim.keymap.set({ "i" }, "<C-K>", function() ls.expand() end, { silent = true })
+vim.keymap.set({ "i", "s" }, "<C-j>", function() ls.jump(1) end, { silent = true })
+vim.keymap.set({ "i", "s" }, "<C-k>", function() ls.jump(-1) end, { silent = true })
+vim.keymap.set({ "i", "s" }, "<C-n>", function() ls.change_choice(1) end, { silent = true })
+vim.keymap.set({ "i", "s" }, "<C-p>", function() ls.change_choice(-1) end, { silent = true })
+
+vim.keymap.set({ "i", "s" }, "<C-e>", function()
+    if ls.choice_active() then
+        ls.change_choice(1)
+    end
+end, { silent = true })
+
 
 -- from: https://github.com/bluz71/dotfiles/blob/master/vim/lua/vsnip-config.lua
 -- local g = vim.g
 -- local fn = vim.fn
-local map = vim.api.nvim_set_keymap
+-- local map = vim.api.nvim_set_keymap
 
--- g.vsnip_snippet_dir = fn.expand('~/dotfiles/vim/vsnip')
+-- -- g.vsnip_snippet_dir = fn.expand('~/dotfiles/vim/vsnip')
 
-local opts = {expr = true, silent = true}
-map('i', '<C-j>', 'vsnip#available(1) ? "<Plug>(vsnip-expand-or-jump)" : "<C-j>"', opts)
-map('i', '<C-k>', 'vsnip#jumpable(-1) ? "<Plug>(vsnip-jump-prev)" : "<C-k>"', opts)
+-- local opts = { expr = true, silent = true }
+-- map('i', '<C-j>', 'vsnip#available(1) ? "<Plug>(vsnip-expand-or-jump)" : "<C-j>"', opts)
+-- map('i', '<C-k>', 'vsnip#jumpable(-1) ? "<Plug>(vsnip-jump-prev)" : "<C-k>"', opts)
 
-local opts = {noremap = true, silent = true}
--- Insert mode snippet completion mapping - '<Control-s>'
-map('i', '<C-s>', '<C-r>=snippets#Complete()<CR>', opts)
+-- local opts = { noremap = true, silent = true }
+-- -- Insert mode snippet completion mapping - '<Control-s>'
+-- map('i', '<C-s>', '<C-r>=snippets#Complete()<CR>', opts)
