7327dd71d59b3b295f8e57d4eb3cde6abb25ecbf
Joao @home <16942018+nnorakk@users.noreply.github.com>
2024-11-04 07:40:47 -0300

    Ajusta espacamento

--- dotfiles/.config/nvim/ftplugin/python.lua
+++ dotfiles/.config/nvim/ftplugin/python.lua
@@ -1,11 +1,11 @@
-vim.wo.colorcolumn='80'
-vim.wo.foldmethod='indent'
-vim.wo.foldlevel=99
+vim.wo.colorcolumn      = '80'
+vim.wo.foldmethod       = 'indent'
+vim.wo.foldlevel        = 99
 
-vim.opt.listchars      = {trail='•',eol='↲', tab='▸\''}
-vim.api.nvim_command('autocmd BufWinEnter *.py normal! zM')
+vim.opt.listchars       = { trail = '•', eol = '↲', tab = '▸\'' }
+-- vim.api.nvim_command('autocmd BufWinEnter *.py normal! zM')
 vim.g.python3_host_prog = '$VIRTUAL_ENV/bin/python3'
-vim.api.nvim_command('autocmd BufWritePre *.py lua vim.lsp.buf.format()')
+-- vim.api.nvim_command('autocmd BufWritePre *.py lua vim.lsp.buf.format()')
 -- vim.cmd [[autocmd BufEnter *.py normal zM]]
 -- vim.cmd [[let g:python3_host_prog = '$VIRTUAL_ENV/bin/python3']]
 -- vim.cmd [[autocmd BufWritePre *.py lua vim.lsp.buf.format()]]
